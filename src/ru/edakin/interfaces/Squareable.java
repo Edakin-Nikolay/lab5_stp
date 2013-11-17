e9083c49859235bea5979b5169b0455d73dd068d
Edakin-Nikolay <nikswat@mail.ru>
2013-11-18 00:04:09 +0700

    first commit

---
+++ src/ru/edakin/interfaces/Squareable.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.edakin.interfaces;
+/**
+ *
+ * @author Nikolay
+ */
+public interface Squareable {
+    double square ();
+}
---
+++ src/ru/edakin/interfaces/Volumable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.edakin.interfaces;
+
+/**
+ *
+ * @author Nikolay
+ */
+public interface Volumable {
+    
+    double volume();   
+}
